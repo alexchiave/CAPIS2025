6541abd89acf3b73ced6460b4f067185acaaf1d1
Filippo Ziche <filippo.ziche@univr.it>
2025-11-10 10:30:43 +0100

    lesson 2

--- 01/exercises/MatrixMultiplication/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIR=`dirname $0`
-
-nvcc -w -std=c++11 -arch=sm_62 "$DIR"/MatrixMultiplication.cu -I"$DIR"/include -o matrix_mul
--- 01/exercises/MatrixTranspose/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIR=`dirname $0`
-
-nvcc -w -std=c++11 -arch=sm_62 "$DIR"/MatrixTranspose.cu -I"$DIR"/include -o mat_transpose
